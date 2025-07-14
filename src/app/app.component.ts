7b032d3317c3bf9e0363aed41c213bdd5e32cca4
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-07-14 21:22:30 +0530

    angular day-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    NavbarComponent,
+    CategoriesComponent,
+    CarouselComponent,
+    BodyComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
