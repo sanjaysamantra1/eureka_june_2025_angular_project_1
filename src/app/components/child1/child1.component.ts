dcccf4b927b5928b98fbcb1f83bb7c8b8130f6b9
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-08-07 01:04:56 +0530

    lifecycle

--- src/app/components/child1/child1.component.ts
+++ src/app/components/child1/child1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-child1',
@@ -6,8 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './child1.component.html',
   styleUrl: './child1.component.css',
   inputs: ['a'],
+  outputs : ['addressEvent']
 })
 export class Child1Component {
   a: any;
   myName = 'Sachin Tendulkar';
+  address = 'Mumbai';
+  addressEvent = new EventEmitter();
+  sendDataToParent() {
+    this.addressEvent.emit(this.address);
+  }
+
+  constructor() {
+    console.log("Child constructor");
+  }
+  ngOnChanges() {
+    console.log('Child ngOnChanges');
+  }
+  ngOnInit() {
+    console.log('Child ngOnInit');
+  }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Child1Component } from '../child1/child1.component';
 import { Child2Component } from '../child2/child2.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-parent',
   imports: [
+    FormsModule,
     Child1Component,
     Child2Component
   ],
@@ -12,5 +14,40 @@ import { Child2Component } from '../child2/child2.component';
   styleUrl: './parent.component.css',
 })
 export class ParentComponent {
-  a = 10;
+  a: number;
+  b: number;
+  parentAddress: string = '';
+  receiveDataFromChild(data: any) {
+    this.parentAddress = data;
+  }
+  constructor() {
+    console.log("Parent constructor");
+    this.a = 10;
+    this.b = 20;
+  }
+  ngOnChanges() {
+    console.log('Parent ngOnChanges');
+  }
+  ngOnInit() {
+    console.log('Parent ngOnInit');
+  }
+  ngDoCheck() {
+    console.log('Parent ngDoCheck');
+  }
+  ngAfterContentInit() {
+    console.log('Parent ngAfterContentInit');
+  }
+  ngAfterContentChecked() {
+    console.log('Parent ngAfterContentChecked')
+  }
+  ngAfterViewInit() {
+    console.log('Parent ngAfterViewInit');
+  }
+  ngAfterViewChecked() {
+    console.log('Parent ngAfterViewChecked');
+  }
+  ngOnDestroy() {
+    console.log('Parent ngOnDestory');
+  }
+
 }
